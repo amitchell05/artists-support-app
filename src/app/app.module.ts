9008e1ab1f231474072791fbb52c91bad4b0327a
Angela Mitchell <anglarenmit@gmail.com>
2021-01-04 20:32:31 -0600

    Deleted slideshowPage component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { SlideshowPageComponent } from './slideshow-page/slideshow-page.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, SlideshowPageComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
